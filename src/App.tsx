14490b8fb606265f883f566cf1216b06050142d3
Julio-Vasquez <jualvalitube@gmail.com>
2024-01-09 23:14:07 -0500

    feat(routes): lazy load and spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 //import Error404 from './components/error404'
 
 import { RouterProvider } from 'react-router-dom'
+import { Suspense } from 'react'
+
 import { GenerateRoutes } from './Routes/routes'
+import { Loader } from './components/loader'
 
 // import { Navbar } from './components/navbar'
 
-const App = () => {
-  const router = GenerateRoutes()
-  return <RouterProvider router={router} />
-}
+const App = () => (
+  <Suspense fallback={<Loader />}>
+    <RouterProvider router={GenerateRoutes()} />
+  </Suspense>
+)
 
 export default App
--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,16 +1,18 @@
-import { ROUTES } from '../constants/url.constant'
+import { lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 
-import Home from '../views/home'
-import Login from '../views/login'
-import Contac from '../views/contact'
-import About from '../views/about/About'
+import { ROUTES } from '../constants/url.constant'
+
+const Home = lazy(() => import('../views/home'))
+const Login = lazy(() => import('../views/login'))
+const Contact = lazy(() => import('../views/contact'))
+const About = lazy(() => import('../views/about'))
 
 export function GenerateRoutes() {
   const routes = createBrowserRouter([
     { path: ROUTES.Home.url, element: <Home /> },
     { path: ROUTES.About.url, element: <About /> },
-    { path: ROUTES.Contact.url, element: <Contac /> },
+    { path: ROUTES.Contact.url, element: <Contact /> },
     { path: ROUTES.Login.url, element: <Login /> },
   ])
 
