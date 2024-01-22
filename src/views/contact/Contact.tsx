3636e8d3fd602f13221477cdb69d2f5fc911f9d7
fernando-pulido <fernandopulido95@icloud.com>
2024-01-21 21:48:53 -0500

    concat view updated

--- src/views/contact/Contact.tsx
+++ src/views/contact/Contact.tsx
@@ -1,5 +1,24 @@
+import './Concat.css'
+import img7 from '../../assets/image/img7.jpg'
 const Contact = () => {
-  return <div>Contact</div>
+  return (
+    <div className='father'>
+      <div className='card'>
+        <img src={img7} className='card-img-top' alt='...' />
+        <div className='card-body'>
+          <h5 className='card-title'>Contactanos a nuestras redes sociales</h5>
+        </div>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
+            Intagram <p>@apologym4</p>
+          </li>
+          <li className='list-group-item'>
+            Facebook <p>apolo gym</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
